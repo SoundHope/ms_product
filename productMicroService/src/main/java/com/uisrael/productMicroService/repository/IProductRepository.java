c5165131c932a335e51314186e7a41311de68a8b
Patricio Ortega <61878128+SoundHope@users.noreply.github.com>
2024-02-09 21:10:40 -0500

    Delete productMicroService directory

--- productMicroService/src/main/java/com/uisrael/productMicroService/repository/IProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uisrael.productMicroService.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.uisrael.productMicroService.model.Product;
-
-public interface IProductRepository extends MongoRepository<Product, String> {
-
-}
--- productMicroService/src/main/java/com/uisrael/productMicroService/repository/IProductTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uisrael.productMicroService.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.uisrael.productMicroService.model.ProductType;
-
-public interface IProductTypeRepository extends MongoRepository<ProductType, String> {
-
-}
